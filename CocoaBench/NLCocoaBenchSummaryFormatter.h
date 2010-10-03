b08cf23e3c52729c1fba380e086dadf9fa93231d
Jonathan Penn <jonathan@wavethenavel.com>
2010-10-03 10:54:09 -0400

    Added url to source

--- CocoaBench/NLCocoaBenchSummaryFormatter.h
+++ CocoaBench/NLCocoaBenchSummaryFormatter.h
@@ -21,6 +21,8 @@
  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  OTHER DEALINGS IN THE SOFTWARE.
+
+ http://github.com/navellabs/cocoa-bench
  */
 
 #import <Foundation/Foundation.h>
